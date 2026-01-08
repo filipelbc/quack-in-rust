56c1a5ed07c54e4dd8512ec928a4fbc681171a65
Filipe Correia <filipelbc@gmail.com>
2026-01-07 22:51:11 -0300

    ld_library_path is only needed when using shared lib

--- run.sh
+++ run.sh
@@ -7,10 +7,12 @@ root_dir=$(dirname "$here")
 
 export DUCKDB_LIB_DIR="$root_dir/duckdb-lib"
 export DUCKDB_INCLUDE_DIR=$DUCKDB_LIB_DIR
-export LD_LIBRARY_PATH=$DUCKDB_LIB_DIR
 
 # comment out if using shared lib
 export DUCKDB_STATIC=1
 export RUSTFLAGS="-C link-arg=-lstdc++"
+
+# uncomment if using shared lib
+#export LD_LIBRARY_PATH=$DUCKDB_LIB_DIR
 
 cargo run
